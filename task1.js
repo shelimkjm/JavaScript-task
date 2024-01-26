b15ef22c120f6441f1c1a6a9b948d2ba99c41e80
shelim <shelimkjm@gmail.com>
2024-01-26 16:02:39 +0600

    task complete

---
+++ task1.js
@@ -0,0 +1,6 @@
+var myMoney = 1000;
+var orangesPrice = 400;
+var applesPrice = 300;
+var totalPrice = orangesPrice + applesPrice;
+var myHaveMoney = myMoney - totalPrice;
+console.log(myHaveMoney);
---
+++ task2.js
@@ -0,0 +1,8 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var english = 99.50;
+var totalMark = mathematics + biology + chemistry + physics + english;
+var averageMark = totalMark / 5;
+console.log(averageMark.toFixed(2));
